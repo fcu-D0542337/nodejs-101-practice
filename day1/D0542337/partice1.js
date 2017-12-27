cc6501c7c008f6b59f7e71aa54665d5c6ffbb24e
fcu-D0542337 <32894181+fcu-D0542337@users.noreply.github.com>
2017-12-27 16:22:14 +0800

    Create partice1.js

---
+++ day1/D0542337/partice1.js
@@ -0,0 +1,22 @@
+{
+  "name": "d0542337",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "homework": "echo 'helloworld'",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "axios": "^0.17.1",
+    "lodash": "^4.17.4"
+  },
+  "devDependencies": {
+    "eslint": "^4.13.1",
+    "jquery": "^3.2.1",
+    "mocha": "^4.0.1",
+    "nodemon": "^1.13.3"
+  }
+}
